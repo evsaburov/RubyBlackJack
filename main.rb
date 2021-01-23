ef11eed09fa12434df4a835caf3b420dd95d9ac2
evs.serv <evs.serv@gmail.com>
2021-01-23 17:06:57 +0500

    work hard

--- main.rb
+++ main.rb
@@ -5,34 +5,6 @@ class Game
 
   SATRT_PLAYER_BALANCE = 100
   SATRT_DILLER_BALANCE = 100
-  CARD_DECK = {
-    "2\u2664" => 2,
-    "3\u2664" => 3,
-    "4\u2664" => 4,
-    "5\u2664" => 5,
-    "6\u2664" => 6,
-    "7\u2664" => 7,
-    "8\u2664" => 8,
-    "9\u2664" => 9,
-    "10\u2664" => 10,
-    "J\u2664" => 2,
-    "2\u2664" => 2,
-    "2\u2664" => 2,
-    "2\u2664" => 2,
-    "2\u2664" => 2,
-    "2\u2664" => 2,
-    "2\u2664" => 2,
-    "2\u2664" => 2,
-    "2\u2664" => 2,
-    "2\u2664" => 2,
-    "2\u2664" => 2,
-    "2\u2664" => 2,
-    "2\u2664" => 2,
-    "2\u2664" => 2,
-    "2\u2664" => 2,
-    "2\u2664" => 2,
-    "2\u2664" => 2,
-  }.freeze
 
   def initialize
     @player_balance = SATRT_PLAYER_BALANCE
@@ -42,10 +14,6 @@ class Game
   def start
 
     while SATRT_PLAYER_BALANCE.positive?
-
-
-
-
 
     end
 
