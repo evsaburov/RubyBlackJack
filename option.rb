7e3f029a1b4c3dced0f2074b6d190aec7356d83b
evs.serv <evs.serv@gmail.com>
2021-01-23 21:30:57 +0500

    work hard

--- option.rb
+++ option.rb
@@ -3,4 +3,12 @@ module Option
     puts 'Введите свое имя:'
     name = gets.chomp.to_s
   end
+
+  def more_game?
+    puts 'Запустить новую игру?(y/n)'
+    more = gets.chomp.to_s
+    result = more == 'y' ? true : false
+  end
+
+  
 end
