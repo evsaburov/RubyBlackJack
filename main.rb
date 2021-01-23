b04c92fa8d0db15c3600f7d8af303b4d63acabe7
evs.serv <evs.serv@gmail.com>
2021-01-23 22:20:10 +0500

    work hard

--- main.rb
+++ main.rb
@@ -18,20 +18,34 @@ class Game
 
       place_bet
       @round = Round.new
+
+
       until round.end?
-
-
-
-        more_game = option.more_game?
-        break unless more_game
+        option.info_round(round)
+        respond = option.player_respond
+        round.player_turn(respond)
       end
+
+      update_balance
+      option.result_round(round)
+
+      more_game = option.more_game?
+      break unless more_game
 
   end
 
   puts "Goodbay #{@player_name}, Balance #{@player_balance}"
 
   def place_bet
-    # code here
+    @player_balance -= 10
+  end
+
+  def update_balance
+    if round.result == 'win'
+      @player_balance += 10
+    elsif round.result == 'lose'
+      @player_balance -= 10
+    end
   end
 
 
